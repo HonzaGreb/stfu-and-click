9bc43d66cc8234d2f00a0b25d74e27a61105763d
Jan Grebennikov <jgrebennikov@gmail.com>
2021-11-14 16:53:24 +0100

    Config Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,28 @@
-module.exports = {}
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', ''],
+  },
+  mode: 'development',
+};
